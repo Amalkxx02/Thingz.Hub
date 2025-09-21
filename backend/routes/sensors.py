df0f0c2c55ef09aeb2bc493c0f8931ffc52d60fa
Amal K <ktkl.amal@gmail.com>
2025-09-21 16:36:51 +0530

    changed sensor table data insertion from sensor data table to direct json

--- backend/routes/sensors.py
+++ backend/routes/sensors.py
@@ -21,35 +21,30 @@ def get_db():           # Dependency generator function to provide a database se
 @router.post("")    # Defines a POST endpoint at the current router’s base path (empty string means no additional path)
 
 # Async endpoint receiving sensor data and a database session via dependency injection
-async def get_data(Sensors_Data: SensorsData, db : Session = Depends(get_db)): # Parameters- variable : pydantic model
+def get_data(Sensors_Data: SensorsData, db : Session = Depends(get_db)): # Parameters- variable : pydantic model
     
     # Query the DevicesTable to find the first device matching the given MAC address and API key
     device = db.query(DevicesTable).filter_by(device_MAC = Sensors_Data.device_MAC, api_key = Sensors_Data.api_key).first() # Check reciving device is registerd
     
     if not device:
-        raise HTTPException(status_code=401, detail="Unauthorized") # Error if not registerd 
+        raise HTTPException(status_code=401, detail="Unauthorized") # Report error if not registerd 
     
     # Create a new SensorsDataTable entry with device MAC and sensor data from the request
     SensorDataEntry = SensorsDataTable(device_MAC=Sensors_Data.device_MAC, data = Sensors_Data.data)
     db.add(SensorDataEntry) # Add the new record to the session
     db.commit()             # Commit the transaction to save it in the database
 
-    insert_data_to_SensorsTable(db)   # Await the asynchronous function to insert data into SensorsTable using the given database session
-
+    insert_data_to_SensorsTable(db, Sensors_Data.device_MAC, Sensors_Data.data)   # Function to insert data into SensorsTable using the given database session
 
     print(f"Received data from {Sensors_Data.device_MAC}: {Sensors_Data.data}")
     return {"status": "ok"}
 
-def insert_data_to_SensorsTable(db:Session):
-    data_in_SensorDataTable = db.query(SensorsDataTable.device_MAC,SensorsDataTable.data).all() # Fetch all device_MAC and data from SensorsDataTable
+def insert_data_to_SensorsTable(db:Session, device_MAC:str, data:dict):
+    for sensors_name in data: # Loop through each sensor name in the data dictionary
+        insert_data = insert(SensorsTable).values(      # Prepare an insert statement with "do nothing" on conflict for unique keys
+        device_MAC = device_MAC,
+        sensors_name = sensors_name
+        ).on_conflict_do_nothing(index_elements=['device_MAC', 'sensors_name'])
 
-    for device_MAC, data in data_in_SensorDataTable:    # Loop through the tupls
-        for sensorName in data: # Loop through the json key for key, value in json.items() to loop also value
-
-            insert_data = insert(SensorsTable).values(      # Prepare an insert statement with "do nothing" on conflict for unique keys
-            device_MAC = device_MAC,
-            sensors_name = sensorName
-            ).on_conflict_do_nothing(index_elements=['device_MAC', 'sensors_name'])
-
-            db.execute(insert_data) # Execute the insert statement
+        db.execute(insert_data) # Execute the insert statement
     db.commit()# Commit the transaction to save it in the database
