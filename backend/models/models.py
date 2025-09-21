a118b31edbd4fdb0528926f4e6db58ec53da5118
Amal K <ktkl.amal@gmail.com>
2025-09-21 15:52:26 +0530

    Created tables for each device sensor and data also setuped the data insertion for each table

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,17 +1,29 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON, UniqueConstraint
 from sqlalchemy.sql import func
-from database import Base
+from database import Base, engine
 
-class DevicesDataBase(Base):
-    __tablename__ = "devices"
-    id = Column(Integer, primary_key=True, index=True)
-    device_id = Column(String, unique=True, nullable=False)
+class DevicesTable(Base):
+    __tablename__ = "Devices_Table"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    device_MAC = Column(String, unique=True, nullable=False)
     api_key = Column(String, unique=True, nullable=False)
+    __table_args__ = (
+        UniqueConstraint("device_MAC", "api_key", name="unique_device_api"),
+    )
 
-class SensorsDataBase(Base):
-    __tablename__ = "sensor_data"
-    id = Column(Integer, primary_key=True, index=True)
-    device_id = Column(String, ForeignKey("devices.device_id"), nullable=False)
+class SensorsDataTable(Base):
+    __tablename__ = "Sensors_Data_Table"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    device_MAC = Column(String, ForeignKey("Devices_Table.device_MAC"), nullable=False)
     data = Column(JSON, nullable=False)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
-    
+
+class SensorsTable(Base):
+    __tablename__ = "Sensors_Table"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    device_MAC = Column(String, ForeignKey("Devices_Table.device_MAC"), nullable=False)
+    sensors_name = Column(String,nullable=False)
+    __table_args__ = (
+        UniqueConstraint("device_MAC", "sensors_name", name="unique_device_sensor"),
+    )
+Base.metadata.create_all(engine)
--- backend/routes/sensors.py
+++ backend/routes/sensors.py
@@ -1,25 +1,55 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import insert
 from schemas.schemas import SensorsData
-from models.models import DevicesDataBase,SensorsDataBase
+from models.models import DevicesTable,SensorsDataTable,SensorsTable
 from database import SessionLocal
 
-router = APIRouter(prefix="/sensors", tags=["sensors"])
+router = APIRouter(prefix="/sensors", tags=["sensors"]) # Creates a router with the base path '/sensors' and groups routes under the 'sensors' tag
 
-def get_db():
-    db = SessionLocal()
+
+def get_db():           # Dependency generator function to provide a database session.
+
+    db = SessionLocal() # Create a new database session
+                        # Session Manages database operations and transactions between the app and the database
+                        # Querying data (reading), Adding new records, Updating existing records, Deleting records
     try:
-        yield db
+        yield db        # Provide the session to the caller
     finally:
-        db.close()
+        db.close()      # Close the session after use
 
-@router.post("") # listener for incoming data on/api/data endpoint from  devices
-async def get_data(sensor_data: SensorsData, db : Session = Depends(get_db)):
-    device = db.query(DevicesDataBase).filter_by(device_id = sensor_data.device_MAC).first()
+@router.post("")    # Defines a POST endpoint at the current router’s base path (empty string means no additional path)
+
+# Async endpoint receiving sensor data and a database session via dependency injection
+async def get_data(Sensors_Data: SensorsData, db : Session = Depends(get_db)): # Parameters- variable : pydantic model
+    
+    # Query the DevicesTable to find the first device matching the given MAC address and API key
+    device = db.query(DevicesTable).filter_by(device_MAC = Sensors_Data.device_MAC, api_key = Sensors_Data.api_key).first() # Check reciving device is registerd
+    
     if not device:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    entry = SensorsDataBase(device_id=sensor_data.device_MAC, data = sensor_data.data)
-    db.add(entry)
-    db.commit()
-    print(f"Received data from {sensor_data.device_MAC}: {sensor_data.data}")
+        raise HTTPException(status_code=401, detail="Unauthorized") # Error if not registerd 
+    
+    # Create a new SensorsDataTable entry with device MAC and sensor data from the request
+    SensorDataEntry = SensorsDataTable(device_MAC=Sensors_Data.device_MAC, data = Sensors_Data.data)
+    db.add(SensorDataEntry) # Add the new record to the session
+    db.commit()             # Commit the transaction to save it in the database
+
+    insert_data_to_SensorsTable(db)   # Await the asynchronous function to insert data into SensorsTable using the given database session
+
+
+    print(f"Received data from {Sensors_Data.device_MAC}: {Sensors_Data.data}")
     return {"status": "ok"}
+
+def insert_data_to_SensorsTable(db:Session):
+    data_in_SensorDataTable = db.query(SensorsDataTable.device_MAC,SensorsDataTable.data).all() # Fetch all device_MAC and data from SensorsDataTable
+
+    for device_MAC, data in data_in_SensorDataTable:    # Loop through the tupls
+        for sensorName in data: # Loop through the json key for key, value in json.items() to loop also value
+
+            insert_data = insert(SensorsTable).values(      # Prepare an insert statement with "do nothing" on conflict for unique keys
+            device_MAC = device_MAC,
+            sensors_name = sensorName
+            ).on_conflict_do_nothing(index_elements=['device_MAC', 'sensors_name'])
+
+            db.execute(insert_data) # Execute the insert statement
+    db.commit()# Commit the transaction to save it in the database
