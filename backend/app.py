d2e7cbdfd4bba19a411f7ce857e332683d7e8203
Amal K <okl.amal.kattumunda@gmail.com>
2025-09-23 15:05:11 +0530

    addedCORSMiddleware in App.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routes import users,devices,sensors,actuators
 
-
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 app.include_router(sensors.router)
 app.include_router(actuators.router)
