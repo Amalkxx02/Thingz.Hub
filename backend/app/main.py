d7fbcd5290e7e483252dae6d5ce9f59fbcc1f521
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2025-10-22 20:30:06 +0530

    Removed the uvicorn and room

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
-from routes import users, devices, rooms, thingz, user_thingz_cards
+from routes import users, devices, thingz, user_thingz_cards,data_handle
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://localhost:5173"]
+origins = ["http://localhost:5173","http://192.168.1.12:5173"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,13 +14,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.include_router(data_handle.router)
 app.include_router(users.router)
 app.include_router(devices.router)
-app.include_router(rooms.router)
+#app.include_router(rooms.router)
 app.include_router(thingz.router)
 app.include_router(user_thingz_cards.router)
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
